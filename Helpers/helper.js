e3e5dc3a1b78c98cb971e14f94286147b85a79ac
Raymond <58218638+raybags-web-dev@users.noreply.github.com>
2023-02-19 21:29:52 +0100

    implimented monthly deletion of all locally stored responses as long as they are in the database

--- Helpers/helper.js
+++ Helpers/helper.js
@@ -1,15 +1,25 @@
 const fs = require('fs')
+const path = require('path')
 
 module.exports = {
   WRITTER: async data => {
     const stream = fs.createWriteStream('output.txt', { flags: 'a' })
     // Write the text to the stream
-    stream.write(`${data}\n ======================`)
+    stream.write(`${data}\n**********************************`)
     // Wait for the stream to finish writing
     stream.on('finish', () => {
-      console.log('Done <=> 😀')
+      console.log('======== Done =======')
     })
     // Close the stream
     stream.end()
+    // Schedule a task to run every month
+    const oneMonth = 30 * 24 * 60 * 60 * 1000 // in milliseconds
+    setInterval(() => {
+      // Delete all text from the "output.txt" file
+      fs.writeFile(path.join(__dirname, 'output.txt'), '', err => {
+        if (err) throw err
+        console.log('File cleared <=> 👌')
+      })
+    }, oneMonth)
   }
 }
