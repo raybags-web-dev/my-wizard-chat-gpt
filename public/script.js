985cf04677f387751c1d5e18bff4ca7ed537ee2c
Raymond <58218638+raybags-web-dev@users.noreply.github.com>
2023-03-02 14:53:47 +0100

    finishing touches on carucel , able to cycle through available dt objects and mapping them to respective DOM elements

--- public/script.js
+++ public/script.js
@@ -7,16 +7,12 @@ const leftContainer = document.querySelector('.inner_left_container')
 const rightCont = document.querySelector('.inner_right_container')
 const outRightContainer = document.querySelector('.right-container')
 const mainLoaderRing = document.querySelector('#main-page-loader')
-
 const searchInput = document.getElementById('seachhhh')
 const searchBtn = document.getElementById('searchBTN')
 const searchFORM = document.getElementById('search_form')
-
 const previousButton = document.querySelector('.get-previous')
 const nextButton = document.querySelector('.get-next')
 let lastScroll = 0
-
-exports = { CONTAINER }
 const loading_1 = document.getElementById('__db_loader')
 GET_loader(loading_1, true)
 
@@ -30,19 +26,18 @@ function GET_loader (element, isFinished) {
 }
 function formatDate (timestamp) {
   const date = new Date(timestamp)
-  const options = { year: 'numeric', month: 'long', day: 'numeric' }
-  return date
-    .toLocaleDateString('en-US', options)
-    .replace(',', '')
-    .replace(/\s+/g, '-')
-    .replace(/\s/g, '')
+  const year = date.getUTCFullYear()
+  const month = String(date.getUTCMonth() + 1).padStart(2, '0')
+  const day = String(date.getUTCDate()).padStart(2, '0')
+  const hours = String(date.getUTCHours()).padStart(2, '0')
+  const minutes = String(date.getUTCMinutes()).padStart(2, '0')
+  return `${year}-${month}-${day},${hours}:${minutes}`
 }
 function QA_HTML (QB, RESPONSE, id) {
   return `
     <div data-id="${id}" class="init-question-response shadow-lg p-2 mb-1 bg-body rounded">
       <p class="init-question" style="position:relative;">
       <img src="./images/face_1.png" style="width:40px;height:40px;position:absolute;bottom:0;right:0;border-radius:50%;padding:.2rem; border:2px solid transparent;box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;" alt="">
-
         ${QB}<br>
         <span class="link fs-6" style="opacity:0">${id}</span>
       </p>
@@ -68,27 +63,27 @@ function DB_QN_RES_HTML (question, response, id, created_at) {
       </p>
     </div>`
 }
-
 function dbItem (item_id, quetion, response, createdAt, updatedAt) {
   return `
     <div id="single_item" data-id="${item_id}"  class="card">
         <div class="card-header d-flex  justify-content-between" style="padding: 1.1rem 4.5rem 1.1rem 4.5rem !important;">
-            <p class="link-success">${item_id}</p>
+            <p data-itemid="timestamp" class="link-success">${item_id}</p>
             <a id="del_BTN" href="#" class="btn btn-danger">DELETE ITEM</a>
+            <span class="DB_Carocel_loader"></span>
         </div>
         <div class="card-body" style="padding: 0 4.5rem 0 4.5rem !important;">
-          <p data-qn="db-qn" class="card-text" style="overflow-y: auto !important; max-height:40%;">Question:&nbsp;${quetion}</p>
+          <p data-qn="db-qn" class="card-text" style="overflow-y: auto !important; max-height:50%;">Question: ${quetion}</p>
           <hr>
-          <p data-res="db-res" class="card-text pb-2"  style="overflow-y: auto !important; max-height:40% !important;">Response:&nbsp;${response}</p>
+          <p data-res="db-res" class="card-text pb-2"  style="overflow-y: auto !important;">Response: ${response}</p>
         </div>
         <div class="d-flex justify-content-between card-footer" style="padding: 1.1rem 4.5rem 1.1rem 4.5rem !important;">
-          <p data-time="db-createdAt">Created:&nbsp;${createdAt}</p>
-          <p data-time="db-modifiedAt" class="floadt-end">Updated:&nbsp;${updatedAt}</p>
+          <p data-time="db-createdAt">Created: ${createdAt}</p>
+          <p data-time="db-modifiedAt" class="floadt-end">Updated: ${updatedAt}</p>
         </div>
         <div data-buttons="carocel-btn-container" class="container-fluid d-flex  justify-content-between align-items-center single-carocel-btn" 
               style="position:fixed;bottom:45%;left:50%;height:2rem;transform:translate(-50%, -50%);z-index:.05;">
-          <span data-check="pre_v" id="prev-btn" style="z-index:1000;font-size:4rem;cursor:pointer;" class="text-black float-start prev">&#60;</span>
-          <span data-check="nex_v" id="next-btn" style="z-index:1000;font-size:4rem;cursor:pointer;" class="text-black float-end next">&#62;</span>
+          <a data-check="pre_v" id="prev-btn" style="z-index:1000;font-size:4rem;cursor:pointer;" class="text-black float-start prev">&#60;</a>
+          <a data-check="nex_v" id="next-btn" style="z-index:1000;font-size:4rem;cursor:pointer;" class="text-black float-end next">&#62;</a>
         </div>
     </div>
     `
@@ -185,9 +180,10 @@ async function fetchDataAndPaginate (previousButton, nextButton) {
             <p class="question" data-id="${_id}">${question}</p>
             <p class="response" data-id="${_id}">${response}</p>
             <span class="link" style="opacity:.5">${_id}</span>
-            <span class="link float-end" style="opacity:.5">${createdAt}</span>
+            <span class="link float-end" style="opacity:.5">${formatDate(
+              createdAt
+            )}</span>
           `
-
           rightCont.insertAdjacentElement(
             'afterbegin',
             dbQuestionResponseElement
@@ -196,7 +192,6 @@ async function fetchDataAndPaginate (previousButton, nextButton) {
           GET_loader(loading_1, true)
         })
     }
-
     // Hide "previous" button on initial page
     previousButton.classList.add('hide')
     // Show/hide pagination buttons based on total number of items
@@ -239,7 +234,6 @@ textArea.addEventListener('keyup', async e => {
     console.info(`Error: ${error.message}`)
   }
 })
-
 async function postFetch (question) {
   if (!question || question == '' || question.length <= 1)
     return updateElementText(`Payload can't be empty message.`, '#error_box')
@@ -260,12 +254,12 @@ async function postFetch (question) {
   }
   try {
     const response = await axios(options)
-    const timestamp = Date.now()
+    const QNtimestamp = Date.now()
     localStorage.setItem('response', JSON.stringify(response))
-    localStorage.setItem('timestamp', JSON.stringify(timestamp))
+    localStorage.setItem('QNtimestamp', JSON.stringify(QNtimestamp))
 
     const QN = await JSON.parse(localStorage.getItem('question'))
-    const time_stamp = await JSON.parse(localStorage.getItem('timestamp'))
+    const time_stamp = await JSON.parse(localStorage.getItem('QNtimestamp'))
     const { data } = await JSON.parse(localStorage.getItem('response'))
 
     if (data.status === 'Success') handlerMainLoader(true)
@@ -321,7 +315,6 @@ async function FetchData (query) {
     console.warn(e.message)
   }
 }
-
 // bring in data
 async function loadLocal () {
   try {
@@ -343,14 +336,24 @@ async function loadLocal () {
   }
 }
 loadLocal()
-
 // token handler
 async function getToken () {
   const storedToken = localStorage.getItem('token')
   const storedTimestamp = localStorage.getItem('timestamp')
   // If a token is stored and less than 24 hours have elapsed since the last request
-  if (storedToken && storedTimestamp && Date.now() - storedTimestamp < 86400000)
+  if (
+    storedToken &&
+    storedTimestamp &&
+    Date.now() - storedTimestamp < 86400000
+  ) {
+    updateElementText(
+      ' Authenticated for the next ( ' +
+        (24 - (new Date().getHours() % 24)) +
+        ' ) HRS',
+      '#error_box'
+    )
     return storedToken
+  }
   // If no token is stored or 24 hours have elapsed
   const response = await axios.post('/raybags/v1/wizard/auth')
   const token = response.data.token
@@ -379,13 +382,13 @@ async function deleteDBItem (e) {
     headers: { Authorization: myToken }
   }
   try {
-    const response = await axios(OPTIONS)
-    if (!response.data.data.length)
+    const DBresponse = await axios(OPTIONS)
+    if (!DBresponse.data.data.length)
       return updateElementText(
         'something went wrong. Please try again later.',
         '#error_box'
       )
-    response.data.data.forEach(item => {
+    DBresponse.data.data.forEach(item => {
       const { createdAt, question, response, _id, updatedAt } = item
 
       if (id == _id) {
@@ -422,6 +425,8 @@ async function deleteDBItem (e) {
             console.log(e.message)
           }
         })
+        handleNextPrev(DBresponse.data.data)
+
         document.addEventListener('keydown', function (event) {
           let itemContainer = document.getElementById('single_item')
           try {
@@ -465,6 +470,7 @@ async function deleteDBItem (e) {
     console.warn(e.message)
   }
 }
+// handle delete
 rightCont.addEventListener('click', deleteDBItem)
 
 async function searchDatabase (e) {
@@ -548,7 +554,6 @@ function updateElementText (message, element_tag) {
   let element = document.querySelector(`${element_tag}`)
   if (!message || !element) return
   let timeoutId
-
   if (document.querySelector('#error_box').innerText !== '') {
     clearInnerText()
   }
@@ -578,6 +583,7 @@ async function typingEffect (element, message, typingSpeed = 4) {
   }
   type()
 }
+// handle search
 searchBtn.addEventListener('click', function (e) {
   searchDatabase(e)
 })
@@ -604,12 +610,79 @@ function handlerMainLoader (isStuffDone) {
     mainLoaderRing.classList.remove('hide')
   }
 }
+async function handleNextPrev () {
+  const myToken = localStorage.getItem('token')
+  const options = {
+    method: 'get',
+    headers: { Authorization: myToken }
+  }
+  const CarucelNextButton = await document.getElementById('next-btn')
+  const CarucelPrevButton = await document.getElementById('prev-btn')
+  let currentITEM = document.querySelector('#single_item').dataset.id
+  options.url = `/raybags/v1/wizard/data-all`
+  let response = await axios(options)
+  let responseData = await response.data.data
+  let currentItemIndex = responseData.findIndex(
+    item => item._id === currentITEM
+  )
+  handleNextPrevUI(responseData[currentItemIndex])
+  // Disable prev button when showing first item
+  if (currentItemIndex === 0) {
+    CarucelPrevButton.disabled = true
+  }
+  CarucelNextButton.addEventListener('click', async () => {
+    if (currentItemIndex < responseData.length - 1) {
+      currentItemIndex++
+      await handleNextPrevUI(responseData[currentItemIndex])
+      // Enable prev button when moving away from first item
+      if (currentItemIndex === 1) {
+        CarucelPrevButton.disabled = false
+      }
+      // Hide next button when showing last item
+      if (currentItemIndex === responseData.length - 1) {
+        CarucelNextButton.style.display = 'none'
+      }
+    }
+  })
+  CarucelPrevButton.addEventListener('click', async () => {
+    if (currentItemIndex > 0) {
+      currentItemIndex--
+      await handleNextPrevUI(responseData[currentItemIndex])
+      // Enable next button when moving away from last item
+      if (currentItemIndex === responseData.length - 2) {
+        CarucelNextButton.style.display = 'block'
+      }
+      // Hide prev button when showing first item
+      if (currentItemIndex === 0) {
+        CarucelPrevButton.disabled = true
+      }
+    }
+  })
+}
+async function handleNextPrevUI (item) {
+  if (!item || item == undefined) return
+  document.querySelector('.DB_Carocel_loader').style.display = 'block'
+  setTimeout(async () => {
+    const { createdAt, updatedAt, question, response, _id } = await item
+    const container = document.getElementById('single_item')
+    const DB_IDD = document.querySelector('[data-itemid="timestamp"]')
+    const DB_QNN = document.querySelector('[data-qn="db-qn"]')
+    const DB_RESS = document.querySelector('[data-res="db-res"]')
+    const DB_MDTIME = document.querySelector('[data-time="db-modifiedAt"]')
+    const DB_CTIME = document.querySelector('[data-time="db-createdAt"]')
+
+    container.setAttribute('data-id', `${_id}`)
+    DB_IDD.textContent = _id
+    DB_QNN.textContent = `Question: ${question}`
+    DB_RESS.textContent = `Response: ${response}`
+    DB_MDTIME.textContent = `Updated: ${formatDate(updatedAt)}`
+    DB_CTIME.textContent = `Created: ${formatDate(createdAt)}`
+    document.querySelector('.DB_Carocel_loader').style.display = 'none'
+  }, 500)
+}
 window.addEventListener('DOMContentLoaded', event => {
   handlerMainLoader(true)
 })
-// Add event listener for form submission
 form.addEventListener('submit', handleSubmit)
 sendButton.addEventListener('click', handleSubmit)
-// authenticate
 authButton.addEventListener('click', getToken)
-// handle submit
